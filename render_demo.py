6db71d33e2b85fcf72d301495b037f7fc8d4a21a
instr3 <instr3@163.com>
2026-06-30 17:20:22 -0400

    Update demo audio outputs

--- render_demo.py
+++ render_demo.py
@@ -3,12 +3,17 @@ import subprocess
 import tempfile
 import shutil
 
-SOUNDFONT_PATH = r'D:\soundfonts\FluidR3_GM.sf2'
+SOUNDFONT_CANDIDATES = [
+    r'D:\soundfonts\FluidR3_GM.sf2',
+    r'D:\Program Files\TiMidityPlusPlus\TimGM6mb.sf2',
+    r'D:\Program Files (x86)\FormatFactory\FFModules\TimGM6mb.sf2',
+]
+SOUNDFONT_PATH = next((path for path in SOUNDFONT_CANDIDATES if os.path.isfile(path)), SOUNDFONT_CANDIDATES[0])
 
 OUTPUT_PATH = 'audio'
 
-def render_midi_to_mp3(midi_file, mp3_file, sr=44100):
-    if os.path.isfile(mp3_file):
+def render_midi_to_mp3(midi_file, mp3_file, sr=44100, overwrite=False):
+    if os.path.isfile(mp3_file) and not overwrite:
         return
     with tempfile.TemporaryDirectory() as temp_dir:
         # copy the midi file to avoid file name encoding issues
@@ -43,13 +48,28 @@ def render_midi_to_mp3(midi_file, mp3_file, sr=44100):
 
 
 def render_all_sheetsage2():
-    midi_path = r'../llama_music/temp/transcriptions2'
-    for suffix in ['fullband', 'vocal']:
-        output_folder = os.path.join(OUTPUT_PATH, 'sheetsage2_V0.2_' + suffix)
+    midi_path = r'../sheetsage2_nolm/temp/sheetsage2_demo_v04'
+    render_specs = [
+        ('melody', 'vocal'),
+        ('full', 'fullsong'),
+    ]
+    for midi_suffix, output_suffix in render_specs:
+        output_folder = os.path.join(OUTPUT_PATH, 'sheetsage2_V0.4_' + output_suffix)
         os.makedirs(output_folder, exist_ok=True)
         for file in os.listdir(midi_path):
-            if file.endswith(f'_{suffix}.mid'):
-                render_midi_to_mp3(os.path.join(midi_path, file), os.path.join(output_folder, file.replace(f'_{suffix}.mid', '.mp3')))
+            if file.endswith(f'_{midi_suffix}.mid'):
+                render_midi_to_mp3(
+                    os.path.join(midi_path, file),
+                    os.path.join(output_folder, file.replace(f'_{midi_suffix}.mid', '.mp3')),
+                    overwrite=True,
+                )
+
+def prepare_midi_versions(file):
+    import pretty_midi
+    midi = pretty_midi.PrettyMIDI(file)
+    midi.write(file[:-9] + '_fullband.mid')
+    midi.instruments[-1].notes.clear()
+    midi.write(file[:-9] + '_vocal.mid')
 
 def render_all_sheetsage():
     midi_path = r'../llama_music/temp/sheetsage_demo'
